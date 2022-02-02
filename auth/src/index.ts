5c71b8efe7302ee5b1c663fa9363a952e319a160
Jan Dyja <jan.dyja@miquido.com>
2022-02-02 22:24:11 +0100

    chore(infra): Adding ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,4 +4,8 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-app.listen(3000, () => console.log('auth service is running'));
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there');
+});
+
+app.listen(3000, () => console.log('auth services is running'));
